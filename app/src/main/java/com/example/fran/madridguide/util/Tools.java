faf7c24d9956c9add6b46fc71b471b8b983be0f4
Francisco Navarro <frn94@live.com>
2017-01-16 13:01:04 +0100

    Download data just after seven days

--- app/src/main/java/com/example/fran/madridguide/util/Tools.java
+++ app/src/main/java/com/example/fran/madridguide/util/Tools.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 
 import com.example.fran.madridguide.R;
 
+import java.util.Date;
+
 /**
  * Created by fran on 16/1/17.
  */
@@ -14,22 +16,22 @@ public class Tools {
     public static void shopDataDownloaded(Context context){
         SharedPreferences sharedPref = context.getSharedPreferences("DOWNLOADED", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putBoolean(context.getString(R.string.SHARED_PREFERENCES_SHOPS_KEY), true);
+        editor.putLong(context.getString(R.string.SHARED_PREFERENCES_SHOPS_KEY), new Date().getTime());
         editor.commit();
     }
     public static void activityDataDownloaded(Context context){
         SharedPreferences sharedPref = context.getSharedPreferences("DOWNLOADED", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putBoolean(context.getString(R.string.SHARED_PREFERENCES_ACTIVITIES_KEY), true);
+        editor.putLong(context.getString(R.string.SHARED_PREFERENCES_ACTIVITIES_KEY), new Date().getTime());
         editor.commit();
     }
 
     public static boolean isActivitiesDownloaded(Context context) {
         SharedPreferences sharedPref = context.getSharedPreferences("DOWNLOADED", Context.MODE_PRIVATE);
-        return sharedPref.getBoolean(context.getString(R.string.SHARED_PREFERENCES_ACTIVITIES_KEY),false);
+        return new Date().getTime() - sharedPref.getLong(context.getString(R.string.SHARED_PREFERENCES_ACTIVITIES_KEY),0) <= 604800000;
     }
     public static boolean isShopsDownloaded(Context context) {
         SharedPreferences sharedPref = context.getSharedPreferences("DOWNLOADED", Context.MODE_PRIVATE);
-        return sharedPref.getBoolean(context.getString(R.string.SHARED_PREFERENCES_SHOPS_KEY),false);
+        return new Date().getTime() - sharedPref.getLong(context.getString(R.string.SHARED_PREFERENCES_SHOPS_KEY),0) <= 604800000;
     }
 }
